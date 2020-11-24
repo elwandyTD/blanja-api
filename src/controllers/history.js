6428aaeaba036e9e10eeab53f7ec9a043ad8781a
Elwandy Tirtana Deriansyah <eltirder0@gmail.com>
2020-11-24 17:27:18 +0700

    update history

--- src/controllers/history.js
+++ src/controllers/history.js
@@ -24,14 +24,16 @@ module.exports = {
 	},
 	insertHistory: async (req, res) => {
 		const {body} = req
-
-		const productPrice = await productModel.getProductById(body.product_id).then((data)=> data[0].product_price)
+		
+		const productPrice = await productModel.getProductById(body.product_id).then((data) => data[0].product_price).catch((e) => e)
+		
 		const insertBody = {
 			...body,
-			history_subtotal: productPrice * body.history_qty,
+			history_subtotal: parseInt(body.product_qty) * productPrice,
 			history_code: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
 			created_at: new Date(Date.now()),
 		}
+		
 		historyModel
 		.insertHistory(insertBody)
 		.then((data) => {
