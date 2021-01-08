756732a03a97da9e02a24d255d8ec1d818daba2d
Elwandy Tirtana Deriansyah <eltirder0@gmail.com>
2021-01-08 18:07:16 +0700

    update error message auth and add nodemailer package

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const logger = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-const mainRouter = require('./src/routes/index')
-const app = express()
-const port = 8000
+const mainRouter = require("./src/routes/index");
+const app = express();
+const port = 8000;
 
 app.listen(port, () => {
-	console.log(`Server is running at port ${port}`)
-})
+  console.log(`Server is running at port ${port}`);
+});
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use(cors())
-app.use(logger('dev'))
+app.use(cors({ origin: true, credentials: true }));
+app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use('/', mainRouter)
+app.use("/", mainRouter);
 
-module.exports = app
+module.exports = app;
--- src/models/auth.js
+++ src/models/auth.js
@@ -1,120 +1,141 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const query = require('../helpers/query')
-const db = require('../configs/mySQL')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const query = require("../helpers/query");
+const db = require("../configs/mySQL");
 
 module.exports = {
-	addNewUser: (body, table) => {
-		return new Promise((resolve, reject) => {
-			db.query(query.queryGet(table, '*', `WHERE user_email='${body.user_email}'`), (err, data) => {
-				if (err) {
-					reject(err)
-				} 
-				if (data.length) {
-					return reject('email sudah terdaftar')
-				}
-				const saltRounds = 10
-				bcrypt.genSalt(saltRounds, (err, salt) => {
-					if (err) {
-						reject(err)
-					}
-					bcrypt.hash(body.user_password, salt, (err, hashedPassword) => {
-						if (err) {
-							reject(err)
-						}
-						const newBody = {
-							...body,
-							user_password: hashedPassword
-						}
-						db.query(query.queryInsert(table), newBody, (err, data) => {
-							if (!err) {
-								resolve(data)
-							} else {
-								reject(err)
-							}
-						})
-					})
-				})
-			})
-		})
-	},
-	checkUserLogin: (body, table) => {
-		return new Promise((resolve, reject) => {
-			const { user_email, user_password } = body
+  addNewUser: (body, table) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        query.queryGet(table, "*", `WHERE user_email='${body.user_email}'`),
+        (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data.length) {
+            return reject("email has been registered");
+          }
+          const saltRounds = 10;
+          bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) {
+              reject(err);
+            }
+            bcrypt.hash(body.user_password, salt, (err, hashedPassword) => {
+              if (err) {
+                reject(err);
+              }
+              const newBody = {
+                ...body,
+                user_password: hashedPassword,
+              };
+              db.query(query.queryInsert(table), newBody, (err, data) => {
+                if (!err) {
+                  resolve(data);
+                } else {
+                  reject(err);
+                }
+              });
+            });
+          });
+        }
+      );
+    });
+  },
+  checkUserLogin: (body, table) => {
+    return new Promise((resolve, reject) => {
+      const { user_email, user_password } = body;
 
-			if ( user_password == '' ) {
-				return reject('silahkan isi password')
-			}
+      if (user_password == "") {
+        return reject("silahkan isi password");
+      }
 
-			db.query(query.queryGet(table, 'user_id, user_password, user_name ', `WHERE user_email='${user_email}'`), (err, data) => {
-				if (err) {
-					reject(err)
-				}
-				// return resolve(data)
-				if (!data.length) {
-					return reject('user tidak ditemukan')
-				}
-				bcrypt.compare(user_password, data[0].user_password, (err, result) => {
-					if (err) {
-						return reject(err)
-					}
-					if (!result) {
-						return reject('password salah')
-					} else {
-						const level = table.slice(0, table.length - 1)
-						const user_id = data[0].user_id
-						console.log(data[0])
-						const payload = {
-							user_name: data[0].user_name,
-							user_email,
-							level,
-						}
-						const secret = process.env.SECRET_KEY;
-						const token = jwt.sign(payload, secret, {
-							expiresIn: "10h"
-						});
-						resolve({
-							token,
-							user_id,
-							user_email,
-							level
-						});
-					}
-				})
-			})
-		})
-	},
-	getToken: (token) => {
-		return new Promise((resolve, reject) => {
-			db.query(query.queryGet('token_whitelist', '*', `WHERE token='${token}'`), (err, data) => {
-				if (!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	},
-	insertToken: (token) => {
-		return new Promise((resolve, reject) => {
-			db.query(query.queryInsert('token_whitelist'), token, (err, data) => {
-				if (!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	},
-	deleteToken: (token) => {
-		return new Promise((resolve, reject) => {
-			db.query(query.queryDelete('token_whitelist', `token='${token}'`), (err, data) => {
-				if (!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	}
-}
+      db.query(
+        query.queryGet(
+          table,
+          "user_id, user_password, user_name ",
+          `WHERE user_email='${user_email}'`
+        ),
+        (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          // return resolve(data)
+          if (!data.length) {
+            return reject("Email or password is not match");
+            // return reject("User not found !");
+          }
+          bcrypt.compare(
+            user_password,
+            data[0].user_password,
+            (err, result) => {
+              if (err) {
+                return reject(err);
+              }
+              if (!result) {
+                return reject("Email or password is not match");
+              } else {
+                const level = table.slice(0, table.length - 1);
+                const user_id = data[0].user_id;
+                console.log(data[0]);
+                const payload = {
+                  user_name: data[0].user_name,
+                  user_email,
+                  level,
+                };
+                const secret = process.env.SECRET_KEY;
+                const token = jwt.sign(payload, secret, {
+                  expiresIn: "10h",
+                });
+                resolve({
+                  token,
+                  user_id,
+                  user_email,
+                  level,
+                });
+              }
+            }
+          );
+        }
+      );
+    });
+  },
+  getToken: (token) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        query.queryGet("token_whitelist", "*", `WHERE token='${token}'`),
+        (err, data) => {
+          if (!err) {
+            resolve(data);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  },
+  insertToken: (token) => {
+    return new Promise((resolve, reject) => {
+      db.query(query.queryInsert("token_whitelist"), token, (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+  deleteToken: (token) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        query.queryDelete("token_whitelist", `token='${token}'`),
+        (err, data) => {
+          if (!err) {
+            resolve(data);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  },
+};
