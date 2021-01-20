e100d4da6a904a4e4d6ae43a841f3a54c5fa9107
Elwandy Tirtana Deriansyah <eltirder0@gmail.com>
2021-01-20 16:02:26 +0700

    update history insert

--- src/controllers/history.js
+++ src/controllers/history.js
@@ -1,61 +1,66 @@
-const historyModel = require('../models/history')
-const cartModel = require('../models/cart')
-const form = require('../helpers/form')
+const historyModel = require("../models/history");
+const cartModel = require("../models/cart");
+const form = require("../helpers/form");
 
 module.exports = {
-	getAllHistory: (_, res) => {
-		historyModel.getAllHistory()
-		.then( async (histories) => {
-			const detailHistories = await historyModel.updateHistoryAttribute(histories)
+  getAllHistory: (_, res) => {
+    historyModel
+      .getAllHistory()
+      .then(async (histories) => {
+        const detailHistories = await historyModel.updateHistoryAttribute(
+          histories
+        );
 
-			form.success(res, detailHistories, 'ambil')
-		}).catch((e) => {
-			form.error(res, e)
-		})
-	},
-	getHistoryById: (req, res) => {
-		const {id} = req.params	
+        form.success(res, detailHistories, "ambil");
+      })
+      .catch((e) => {
+        form.error(res, e);
+      });
+  },
+  getHistoryById: (req, res) => {
+    const { id } = req.params;
 
-		historyModel
-		.getHistoryById(id)
-		.then( async (history) => {
-			const detailHistories = await historyModel.updateHistoryAttribute(history)
+    historyModel
+      .getHistoryById(id)
+      .then(async (history) => {
+        const detailHistories = await historyModel.updateHistoryAttribute(
+          history
+        );
 
-			form.success(res, detailHistories, 'ambil')
-		}).catch((e) => {
-			form.error(res, e)
-		})
-	},
-	insertHistory: async (req, res) => {
-		const {body} = req
-		
-		// const totalProduct = await cartModel.getTotalCartByUserId(body.user_id).then((total) => total[0].product_total).catch((e) => res.json(e))
-		const insertBody = {
-			...body,
-			// history_subtotal: totalProduct,
-			// history_subtotal: body.product_price,
-			history_code: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-			created_at: new Date(Date.now()),
-		}
-		
-		historyModel
-		.insertHistory(insertBody)
-		.then((data) => {
-			const id = data.insertId
-			
-			// historyModel
-			// .insertSelectProductUser(parseInt(body.user_id), id)
-			// .then(() => {
-				const resObj = {
-					msg: 'sukses',
-					data: {id: id, ...insertBody}
-				}
-				res.json(resObj);
-			// }).catch((e) => {
-			// 	form.error(res, e)
-			// })
-		}).catch((e) => {
-			form.error(res, e)
-		})
-	},
-}
+        form.success(res, detailHistories, "ambil");
+      })
+      .catch((e) => {
+        form.error(res, e);
+      });
+  },
+  insertHistory: async (req, res) => {
+    const { body } = req;
+
+    const historyBody = {
+      user_id: body.user_id,
+      history_code: Math.random().toString(36).substring(2, 15),
+      history_address: body.address,
+      history_method: body.method,
+      history_subtotal: body.subtotal,
+      created_at: new Date(Date.now()),
+    };
+
+    historyModel
+      .insertHistory(historyBody)
+      .then((history) => {
+        // console.log(body.items);
+        // console.log(history.insertId);
+        historyModel
+          .insertDetailHistory(body.items, history.insertId)
+          .then((data) => {
+            res.send(data);
+          })
+          .catch((err) => {
+            res.send({ err });
+          });
+      })
+      .catch((err) => {
+        res.send({ err });
+      });
+  },
+};
--- src/models/history.js
+++ src/models/history.js
@@ -1,75 +1,104 @@
-const db = require('../configs/mySQL')
-const qs = require('../helpers/query')
+const db = require("../configs/mySQL");
+const qs = require("../helpers/query");
 
 const getDetailByHistoryId = (id) => {
-	return new Promise((resolve, reject) => {
-		db.query(qs.queryGet('detail_histories', '*',`WHERE history_id=${id}`), (err, data) => {
-			if(!err) {
-				resolve(data)
-			} else {
-				reject(err)
-			}
-		})
-	})
-}
+  return new Promise((resolve, reject) => {
+    db.query(
+      qs.queryGet("detail_histories", "*", `WHERE history_id=${id}`),
+      (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      }
+    );
+  });
+};
 
 module.exports = {
-	getAllHistory: () => {
-		return new Promise((resolve, reject) => {
-			db.query(qs.queryGet('histories', '*'), (err, data) => {
-				if(!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	},
-	updateHistoryAttribute: async(data) => {
-		try {
-			for (let i = 0; i < data.length; i++) {
-				const detailItems = await getDetailByHistoryId(data[i].history_id)
+  getAllHistory: () => {
+    return new Promise((resolve, reject) => {
+      db.query(qs.queryGet("histories", "*"), (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+  updateHistoryAttribute: async (data) => {
+    try {
+      for (let i = 0; i < data.length; i++) {
+        const detailItems = await getDetailByHistoryId(data[i].history_id);
 
-				data[i].products = detailItems
-			}
-			return data
-		} catch(e) {
-			return e
-		}
-	},
-	getHistoryById: (id) => {
-		return new Promise((resolve, reject) => {
-			db.query(qs.queryGet('histories', '*',`WHERE history_id=${id}`), (err, data) => {
-				if(!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	},
-	insertHistory: (body) => {
-		return new Promise((resolve, reject) => {
-			const queryS = 'INSERT INTO histories SET ?'
-			db.query(queryS, body, (err, data) => {
-				if(!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	},
-	insertSelectProductUser: (user_id, history_id) => {
-		return new Promise((resolve, reject) => {
-			const queryS = `INSERT INTO detail_histories(history_id, product_id, product_color_id, product_size_id, product_qty, product_subtotal) SELECT ${history_id},product_id,product_color_id,product_size_id,product_qty,product_subtotal FROM cart WHERE user_id=${user_id}`
-			db.query(queryS, (err, data) => {
-				if(!err) {
-					resolve(data)
-				} else {
-					reject(err)
-				}
-			})
-		})
-	}
-}
+        data[i].products = detailItems;
+      }
+      return data;
+    } catch (e) {
+      return e;
+    }
+  },
+  getHistoryById: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        qs.queryGet("histories", "*", `WHERE history_id=${id}`),
+        (err, data) => {
+          if (!err) {
+            resolve(data);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  },
+  insertHistory: (body) => {
+    return new Promise((resolve, reject) => {
+      const queryS = "INSERT INTO histories SET ?";
+      db.query(queryS, body, (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+  insertDetailHistory: (body, id) => {
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < body.length; i++) {
+        const insert = {
+          product_id: body[i].product_id,
+          product_title: body[i].product_title,
+          brand_name: body[i].brand_name,
+          product_image: body[i].img,
+          product_color: body[i].color,
+          product_size: body[i].size,
+          product_qty: body[i].qty,
+          product_subtotal: body[i].qty * body[i].product_price,
+        };
+        const queryS = `INSERT INTO detail_histories SET history_id=${id}, ?`;
+        db.query(queryS, insert, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+        });
+      }
+      resolve({ msg: "success" });
+    });
+  },
+  insertSelectProductUser: (user_id, history_id) => {
+    return new Promise((resolve, reject) => {
+      const queryS = `INSERT INTO detail_histories(history_id, product_id, product_color_id, product_size_id, product_qty, product_subtotal) SELECT ${history_id},product_id,product_color_id,product_size_id,product_qty,product_subtotal FROM cart WHERE user_id=${user_id}`;
+      db.query(queryS, (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+};
